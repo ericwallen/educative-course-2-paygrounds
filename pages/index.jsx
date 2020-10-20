8cbfa35806d5c9eac2192f327a00af9d5ca75bf2
Eric Wallen <ericwallen1@gmail.com>
2020-10-20 11:58:45 -0600

    commit

---
+++ pages/index.jsx
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import {useEffect, useState} from 'react'
+
+export default function Home(initialData) {
+  const [formInputs, setFormInputs] = useState({})
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(()=>{
+    setSearchResults(initialData.catGiphys.data)
+  }, [initialData])
+
+  const handleInputs = (event) => {
+    let {name, value} = event.target
+    setFormInputs({ ...formInputs, [name]: value });
+  }
+
+  const search = (event) => {
+    event.preventDefault()
+    console.log(formInputs.searchTerm);
+  }
+
+  return (
+    <div className='container'>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/styles.css"/>
+      </Head>
+
+      <h1>Giphy Search App</h1>
+
+      <form onSubmit={search}>
+        <input name="searchTerm" onChange={handleInputs} type="text" required />
+        <button>Search</button>
+      </form>
+
+      <div className="giphy-search-results-grid">
+        {searchResults.map((each, index) => {
+          return(
+            <div key={index}>
+              <h3>{each.title}</h3>
+              <img src={each.images.original.url} alt={each.title}/>
+            </div>
+          )
+        })}
+      </div>
+      
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  let catGiphys = await fetch('https://api.giphy.com/v1/gifs/search?q=cats&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6')
+  catGiphys = await catGiphys.json()
+  return {props: {catGiphys: catGiphys}}  
+}
